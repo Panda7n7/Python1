85af265252d324f30e2baac49a722835970802f9
Panda <Panda7n7@163.com>
2024-04-22 15:23:23 +0800

    first commit

---
+++ test.py
@@ -0,0 +1,7 @@
+import print_functions as pf
+
+unprinted_model = ['iphone case', 'robot pendant', 'dodecahedron']
+completed_model = []
+pf.print_models(unprinted_model, completed_model)
+pf.show_completed_models(unprinted_model)
+pf.show_completed_models(completed_model)
